16cf64b654df3609dd79e075507195cdd7e1533a
Nidish <nidbid@gmail.com>
2017-03-19 14:49:57 +0530

    Maxima tutorial completed

---
+++ Maxima/auto/MaximaTut_IISTFOSS.el
@@ -0,0 +1,33 @@
+(TeX-add-style-hook
+ "MaximaTut_IISTFOSS"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "12pt" "usenames" "pdftex")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("inputenc" "utf8") ("emaxima" "lines" "breqn")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer12"
+    "fontenc"
+    "inputenc"
+    "graphics"
+    "lmodern"
+    "emaxima"
+    "url"
+    "amsmath"
+    "bm"
+    "listings"))
+ :latex)
+
